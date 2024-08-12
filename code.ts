8de2c225c0a010daeef4b5f29717fe0e8d100cda
Roman Nebel <im.roman.nebel@gmail.com>
2024-08-12 14:02:22 +0200

    Add basic reccursive reactions selector

--- code.ts
+++ code.ts
@@ -1,22 +1,43 @@
-// This plugin creates 5 rectangles on the screen.
-const numberOfRectangles = 5
+const frames = figma.currentPage.selection as FrameNode[];
 
-// This file holds the main code for plugins. Code in this file has access to
-// the *figma document* via the figma global object.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser environment (See https://www.figma.com/plugin-docs/how-plugins-run).
+const variants: any = [];
 
-const nodes: SceneNode[] = [];
-for (let i = 0; i < numberOfRectangles; i++) {
-  const rect = figma.createRectangle();
-  rect.x = i * 150;
-  rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-  figma.currentPage.appendChild(rect);
-  nodes.push(rect);
+function getReactions(nodes: FrameNode[]) {
+  for (const node of nodes) {
+    for (const reaction of node.reactions) {
+      variants.push({
+        targetFrame: node.id,
+        action: reaction.action,
+      });
+    }
+
+    if (node.children) {
+      getReactionsRecursive(node.children as FrameNode[], node);
+    }
+  }
 }
-figma.currentPage.selection = nodes;
-figma.viewport.scrollAndZoomIntoView(nodes);
 
-// Make sure to close the plugin when you're done. Otherwise the plugin will
-// keep running, which shows the cancel button at the bottom of the screen.
+function getReactionsRecursive(nodes: FrameNode[], baseNode: FrameNode) {
+  for (const node of nodes) {
+    for (const reaction of node.reactions) {
+      if (
+        reaction.action?.type === "NODE" &&
+        reaction.action?.navigation === "OVERLAY"
+      )
+        variants.push({
+          targetFrame: baseNode.id,
+          action: reaction.action,
+        });
+    }
+
+    if (node.children) {
+      getReactionsRecursive(node.children as FrameNode[], baseNode);
+    }
+  }
+}
+
+getReactions(frames);
+
+console.log(variants);
+
 figma.closePlugin();
