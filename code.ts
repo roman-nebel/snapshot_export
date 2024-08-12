965afddce97ec02201fe403569e5a3e88edcd183
Roman Nebel <im.roman.nebel@gmail.com>
2024-08-12 20:32:05 +0200

    Update main flow

--- code.ts
+++ code.ts
@@ -1,43 +1,105 @@
 const frames = figma.currentPage.selection as FrameNode[];
 
-const variants: any = [];
+type VariantType = {
+  triggerNodeId: string;
+  action: Action;
+};
 
-function getReactions(nodes: FrameNode[]) {
-  for (const node of nodes) {
-    for (const reaction of node.reactions) {
-      variants.push({
-        targetFrame: node.id,
-        action: reaction.action,
-      });
-    }
+type VariantsType = {
+  [key: string]: VariantType[];
+};
 
-    if (node.children) {
-      getReactionsRecursive(node.children as FrameNode[], node);
-    }
+const variants: VariantsType = {};
+
+function addItem(key: string, item: VariantType) {
+  if (!variants[key]) {
+    variants[key] = [];
   }
+  variants[key].push(item);
 }
 
-function getReactionsRecursive(nodes: FrameNode[], baseNode: FrameNode) {
+function getReactionsRecursive(nodes: FrameNode[], baseNodeId: string) {
   for (const node of nodes) {
     for (const reaction of node.reactions) {
       if (
+        reaction.action &&
         reaction.action?.type === "NODE" &&
         reaction.action?.navigation === "OVERLAY"
       )
-        variants.push({
-          targetFrame: baseNode.id,
+        addItem(baseNodeId, {
+          triggerNodeId: node.id,
           action: reaction.action,
         });
     }
 
     if (node.children) {
-      getReactionsRecursive(node.children as FrameNode[], baseNode);
+      getReactionsRecursive(node.children as FrameNode[], baseNodeId);
     }
   }
 }
 
+async function getReactions(nodes: FrameNode[]) {
+  for (const node of nodes) {
+    for (const reaction of node.reactions) {
+      if (reaction.action) {
+        addItem(node.id, {
+          triggerNodeId: node.id,
+          action: reaction.action,
+        });
+      }
+    }
+
+    if (node.children) {
+      getReactionsRecursive(node.children as FrameNode[], node.id);
+    }
+
+    for (const { triggerNodeId, action } of variants[node.id]) {
+      const triggeredNode = (await figma.getNodeByIdAsync(
+        triggerNodeId
+      )) as SceneNode;
+      const addingNode = (await figma.getNodeByIdAsync(
+        action.destinationId || ""
+      )) as SceneNode;
+      if (addingNode) {
+        const nodeAbsoluteX = node.absoluteBoundingBox?.x || 0;
+        const nodeAbsoluteY = node.absoluteBoundingBox?.y || 0;
+        const overlayAbsoluteX = triggeredNode.absoluteBoundingBox?.x || 0;
+        const overlayAbsoluteY = triggeredNode.absoluteBoundingBox?.y || 0;
+        const newAddingNode = addingNode.clone() as FrameNode;
+        node.appendChild(newAddingNode);
+        if (node.layoutMode !== "NONE")
+          newAddingNode.layoutPositioning = "ABSOLUTE";
+        newAddingNode.x =
+          overlayAbsoluteX -
+            nodeAbsoluteX +
+            action.overlayRelativePosition?.x || 0;
+        newAddingNode.y =
+          overlayAbsoluteY -
+            nodeAbsoluteY +
+            action.overlayRelativePosition?.y || 0;
+        const imageRaw = await node.exportAsync({ format: "PNG" });
+        const image = figma.createImage(imageRaw);
+
+        const width = node.width;
+        const height = node.height;
+
+        const rect = figma.createRectangle();
+        rect.resize(width, height);
+
+        const fill: Paint = {
+          type: "IMAGE",
+          scaleMode: "FIT",
+          imageHash: image.hash,
+        };
+
+        rect.fills = [fill];
+
+        figma.currentPage.appendChild(rect);
+        newAddingNode.remove();
+      }
+    }
+  }
+  figma.closePlugin();
+}
+
 getReactions(frames);
-
-console.log(variants);
-
-figma.closePlugin();
